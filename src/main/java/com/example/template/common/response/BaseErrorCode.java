226d9fbd0f62365c06935521e3450adfef637c80
YamYamee <82762402+YamYamee@users.noreply.github.com>
2024-12-26 17:14:07 +0900

    CI/CD 및 초기 설정 (#1)

---
+++ src/main/java/com/example/template/common/response/BaseErrorCode.java
@@ -0,0 +1,8 @@
+package com.example.template.common.response;
+
+public interface BaseErrorCode {
+
+    public ErrorReasonDTO getReason();
+
+    public ErrorReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/com/example/template/domain/example/service/ExampleService.java
@@ -0,0 +1,5 @@
+package com.example.template.domain.example.service;
+
+public interface ExampleService {
+
+}
