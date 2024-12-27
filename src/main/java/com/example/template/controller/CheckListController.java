c218e2df4fd02015ff462407b5136d964eebd794
YamYamee <cartcnx@naver.com>
2024-12-28 05:09:26 +0900

    .

--- src/main/java/com/example/template/controller/CheckListController.java
+++ src/main/java/com/example/template/controller/CheckListController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/checklist")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class CheckListController {
 
     final CheckListService checkListService;
